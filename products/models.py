654b038437849ed955305a2352b1d629fa58ebfa
shivam <shivshopextra011@gmai.com>
2020-04-26 01:48:09 +0530

    AddedCreate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Product(models.Model):
 
-# Create your models here.
+    titles=models.CharField(max_length=200)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    url=models.TextField()
+    image=models.ImageField(upload_to='image/')
+    icon = models.ImageField(upload_to='image/')
+    votes_total= models.IntegerField(default=1)
+    hunter=models.ForeignKey(User,on_delete=models.CASCADE) #If user deleted product also deleted
+
+    def __str__(self):
+        return self.titles
+
+    def summary(self):
+        return self.body[:1000]
+    def pub_date_modified(self):
+        return self.pub_date.strftime(' %b %e %Y')

69031ef768a05519f0f54808ef26bfda12abf6b5
shivam <shivshopextra011@gmai.com>
2020-07-31 19:49:07 +0530

    SixthCommit

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,6 +94,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 
